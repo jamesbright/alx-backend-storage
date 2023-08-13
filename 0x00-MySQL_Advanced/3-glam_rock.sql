588dd3c04e9924ab277b2f21bb7c63567d5e96b7
jamesbright <jamesononiwu@gmail.com>
2023-08-13 02:46:13 +0100

    correct glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 
 SELECT
     band_name,
-    IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+    IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%'
 ORDER BY 2 DESC;
