6f604c902b3cee8356eb21b7c05547d7abf0e54d
jamesbright <jamesononiwu@gmail.com>
2023-08-13 01:53:39 +0100

    added create user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- Make a table
+
+-- If table exist, don't fail.
+
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        PRIMARY KEY(id)
+    );
