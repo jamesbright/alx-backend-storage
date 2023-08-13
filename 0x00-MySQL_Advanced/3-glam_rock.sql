66a3d33ee64d734a6e394f077bed4aadf2c03681
jamesbright <jamesononiwu@gmail.com>
2023-08-13 02:34:06 +0100

    glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- Search bands with style Glam rock
+
+-- Duration current
+
+SELECT
+    band_name,
+    IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
