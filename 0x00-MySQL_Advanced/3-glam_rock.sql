c98ce5a07d260556945537c4c03c14430ec4667f
jamesbright <jamesononiwu@gmail.com>
2023-08-13 02:36:29 +0100

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,8 @@
--- Search bands with style Glam rock
-
--- Duration current
+-- lists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT
     band_name,
     IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+WHERE style like '%Glam rock%'
+ORDER BY 2 DESC;
