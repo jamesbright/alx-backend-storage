1774c7fba0ae6c6b7a72c060b4d756720e3ff535
jamesbright <jamesononiwu@gmail.com>
2023-08-13 02:49:11 +0100

    trigger orders

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,9 @@
 -- creates a trigger that decreases the quantity of an item after adding a new order
 
 CREATE TRIGGER DECREASE_QTY 
-	decrease_qty after
-	insert ON orders for each row
+	after insert ON orders 
+    for each row
 	update items
-	SET
-	    quantity = quantity - NEW.number
+	SET quantity = quantity - NEW.number
 	WHERE NEW.item_name = name;
 
