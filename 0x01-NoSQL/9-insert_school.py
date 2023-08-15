c5b25525da011a4de0e183d830edbca2c934b87d
jamesbright <jamesononiwu@gmail.com>
2023-08-15 02:46:23 -0700

    insert into school

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" MongoDB insert """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that inserts a document in school collection"""
+    if len(kwargs) == 0:
+        return None
+    return mongo_collection.insert(kwargs)
+
